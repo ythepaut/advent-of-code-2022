cca85a2b0e902b89a13c9909c242fa7e5ec904ed
Yohann THEPAUT <contact@ythepaut.com>
2022-12-03 12:43:42 +0100

    Add day 1 solution

--- solutions/day1.py
+++ solutions/day1.py
@@ -4,18 +4,46 @@ Yohann THEPAUT (ythepaut) <contact@ythepaut.com>
 """
 
 
-def preprocess_inputs(inputs: list) -> None:
-    pass
+def preprocess_inputs(inputs: list[str]) -> list[list[int]]:
+    elves: list[list[int]] = []
+    current_elf: list[int] = []
+    for input_ in inputs:
+        if input_ == "\n":
+            elves.append(current_elf)
+            current_elf = []
+        else:
+            current_elf.append(int(input_[:-1]))
+    if len(current_elf) > 0:
+        elves.append(current_elf)
+    return elves
 
 
-def part1(inputs: list) -> int:
-    pass
+def elf_with_max_calories(elves: list[list[int]]) -> int:
+    """Returns the index of the elf carrying the most snacks"""
+    assert len(elves) > 0
+    max_calories: int = sum(elves[0])
+    best_elf_index: int = 0
+    for index, elf in enumerate(elves[1:]):
+        calories: int = sum(elf)
+        if calories > max_calories:
+            max_calories = calories
+            best_elf_index = index + 1
+    return best_elf_index
 
 
-def part2(inputs: list) -> int:
-    pass
+def part1(elves: list[list[int]]) -> int:
+    return sum(elves[elf_with_max_calories(elves)])
 
 
-def solve(inputs: list) -> tuple[int, int]:
-    preprocess_inputs(inputs)
-    return part1(inputs), part2(inputs)
+def part2(elves: list[list[int]]) -> int:
+    total_calories: int = 0
+    for i in range(3):
+        best_elf: int = elf_with_max_calories(elves)
+        total_calories += sum(elves[best_elf])
+        elves.pop(best_elf)
+    return total_calories
+
+
+def solve(inputs: list[str]) -> tuple[int, int]:
+    elves = preprocess_inputs(inputs)
+    return part1(elves), part2(elves)
